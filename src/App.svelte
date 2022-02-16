68c55276a9d100788e31fbe43544f00b34830775
Sarthak Jha <sarthakjha94@gmail.com>
2022-02-16 21:55:19 +0530

    added file for testing

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,14 @@
 <script>
+    import Header from "./components/Header.svelte";
 </script>
 
-<main>
-  <h1>Hello world!</h1>
-
-</main>
+<div>
+    <Header />
+    <h1>Hello world!</h1>
+    <h2> THis is just amazing</h2>
+    <h1>This is just another heading</h1>
+</div>
 
 <style>
+
 </style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<header>
+    <ul>
+        <a href="#"><li>Test</li></a>
+        <a href="#"><li>Contact</li></a>
+    </ul>
+</header>
+
+<style>
+    
+</style>
